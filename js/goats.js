bbb887161b77bcbb64bf55453a6356631ca82116
Edson Rodrigues <thecodeverse12@gmail.com>
2025-01-14 11:35:11 +0100

    fetching user data from github api

--- js/goats.js
+++ js/goats.js
@@ -1,5 +1,5 @@
-// Logica dos dados
-// Estruturacao dos dados
+import { Githubuser } from "./githubuser.js";
+
 export class Goats {
     constructor(root) {
         this.root = document.querySelector(root);
@@ -10,10 +10,38 @@ export class Goats {
         this.data = JSON.parse(localStorage.getItem('@github-goats:')) || [];
     }
 
+    save() {
+        localStorage.setItem('@github-goats:', JSON.stringify(this.data));
+    }
+
+    async add(username) {
+        try {
+
+            const userExists = this.data.find(entry => entry.login === username);
+
+            if(userExists) {
+                throw new Error('User already exists');
+            }
+
+            const user = await Githubuser.search(username); 
+            
+            if(user.login === undefined) {
+                throw new Error('Usuario nao encontrado')
+            }
+
+            this.data = [user, ...this.data];
+            this.update();
+            this.save();
+
+        } catch(error) {
+            alert(error.message);
+        }
+    }
+
     delete(target) {
-        this.data = this.data.filter(item => (item.username !== target.username));
-        console.log(this.data);
+        this.data = this.data.filter(item => (item.login !== target.login));
         this.update();
+        this.save();
     }
 }
 
@@ -22,6 +50,16 @@ export class GoatsView extends Goats {
         super(root)
         this.tbody = this.root.querySelector('table tbody');
         this.update();
+        this.onadd();
+    }
+
+    onadd(){
+        const addButton = this.root.querySelector('.search button');
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input');
+
+            this.add(value);
+        }
     }
 
     update() {
@@ -47,10 +85,10 @@ export class GoatsView extends Goats {
 
         tr.innerHTML = `
                     <td class="user">
-                        <img src="https://github.com/${item.username}.png" alt="Profile picture">
-                        <a href="https://github.com/${item.username}" target="_blank">
+                        <img src="https://github.com/${item.login}.png" alt="Profile picture">
+                        <a href="https://github.com/${item.login}" target="_blank">
                             <p>${item.name}</p>
-                            <span>${item.username}</span>
+                            <span>${item.login}</span>
                         </a>
                     </td>
                     <td class="repositories">
